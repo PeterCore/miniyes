36bb7d590d6bbd7d309258f48971b517829fafd6
zhangchun <zhangchun@oamicnet.com>
2025-02-11 02:09:46 +0800

    init

---
+++ src/pages/course-manager/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="h-full w-full flex flex-col">
+    <view class="w-full flex flex-row items-center justify-between border-b border-gray-200 bg-white py-3">
+      <view class="w-15" />
+      <view class="w-full flex flex-row items-center justify-start">
+        <u-icon name="list-dot" color="text-orange-500" size="24" />
+        <view class="text-30">
+          课程列表
+        </view>
+      </view>
+      <view class="w-full" />
+      <button class="add-course-btn" @tap="pushCreateCoursePage">
+        + 新增课程
+      </button>
+      <view class="w-20" />
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+const pushCreateCoursePage = () => {
+  uni.navigateTo({ url: '/pages/create-course/index' });
+};
+</script>
+
+<style lang="scss" scoped>
+.add-course-btn {
+  @apply flex-grow flex-shrink-0 flex-row mr-[10rpx] items-center  justify-center h-[60rpx]  rounded-lg text-25rpx bg-green-500 border-none  text-white  hover:bg-green-600;
+}
+</style>
+
+<!-- <style scoped></style> -->
---
+++ src/pages/create-course/index.vue
@@ -0,0 +1,179 @@
+<template>
+  <view class="page-wrap">
+    <view
+      class="w-full flex flex-row items-center justify-start border-b border-gray-200 bg-white py-3"
+    >
+      <view class="w-30" />
+      <view class="w-full flex flex-row items-center justify-start">
+        <u-icon name="calendar" color="#f97316" size="24" />
+        <view class="text-30">
+          课程信息
+        </view>
+      </view>
+      <view class="course-title">
+        填写课程相关信息
+      </view>
+    </view>
+
+    <view
+      class="mt-[10rpx] w-full flex flex-row items-center justify-start border-b border-gray-200 bg-white py-3"
+    >
+      <view class="w-30" />
+      <view class="mr-[10rpx] w-[180rpx] items-center text-30 color-#333">
+        课程名称
+      </view>
+      <view class="w-full">
+        <input
+          v-model="value"
+          placeholder="请输入课程名称(英语，数学，语文)"
+          placeholderStyle="font-size:9 color: #ededed"
+          border="surround"
+          @change="edit_course"
+        >
+      </view>
+    </view>
+
+    <view
+      class="mt-[10rpx] w-full flex flex-row items-center justify-start border-b border-gray-200 bg-white py-3"
+    >
+      <view class="w-30" />
+      <view class="mr-[10rpx] w-[280rpx] items-center text-30 color-#333">
+        默认上课时长(分钟)
+      </view>
+      <view class="flex-grow" />
+      <view class="edit-course-time">
+        <button class="btn" @click="increased">
+          +
+        </button>
+        <view class="centered-input">
+          <input
+            v-model="duration"
+            type="number"
+            border="surround"
+            @change="edit_duration"
+          >
+        </view>
+        <button class="btn" @click="decreased">
+          -
+        </button>
+      </view>
+      <view class="w-15" />
+    </view>
+
+    <view
+      class="mt-[10rpx] flex flex-row items-center justify-start border-b border-gray-200 bg-white py-3"
+    >
+      <view class="w-30" />
+      <view class="w-[180rpx] items-center text-30 color-#333">
+        课时费用
+      </view>
+      <view class="w-[80rpx]">
+        <input
+          v-model="cnyValue"
+          placeholder="请输入课时费用"
+          placeholderStyle="font-size:9 color: #ededed"
+          border="surround"
+          type="number"
+          @change="edit_cnyValue"
+        >
+      </view>
+      <view class="mr-[10rpx] w-[10rpx] items-center text-30 color-#333">
+        元
+      </view>
+    </view>
+
+    <view class="mt-20rpx bg-white">
+      <u-cell-group>
+        <u-cell title="课程类型" :value="course" is-link @click="pickCourses" />
+      </u-cell-group>
+    </view>
+    <u-picker :show="showPicker" :columns="columns" @confirm="confirm" @cancel="cancel" />
+  </view>
+  <view class="mx-[30rpx] mt-40rpx">
+    <up-button type="primary" :disabled="disabled" text="提交" @click="submit" />
+  </view>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const value = ref('');
+const cnyValue = ref(80);
+const duration = ref(20);
+const course = ref('英语');
+const showPicker = ref(false);
+const disabled = ref(false);
+
+const columns = reactive([
+  ['英语', '语文', '数学'],
+]);
+const increased = () => {
+  // +
+  duration.value++;
+};
+
+const edit_cnyValue = (e: any) => {
+  const inputValue = e.detail.value;
+  const _cny = Number.isNaN(Number(inputValue)) ? 0 : Number(inputValue);
+  cnyValue.value = _cny;
+};
+
+const submit = () => {
+  uni.showLoading({ title: '提交中' });
+  disabled.value = true;
+};
+
+const pickCourses = () => {
+  showPicker.value = true;
+};
+const confirm = (e: any) => {
+  console.log('confirm', e);
+  course.value = e.value[0];
+  showPicker.value = false;
+};
+
+const cancel = () => {
+  showPicker.value = false;
+};
+
+async function decreased() {
+  // -
+  if (duration.value === 20) {
+    uni.$u.toast('时长不能小于20分钟');
+    return;
+  }
+  duration.value--;
+}
+
+async function edit_course(e: any) {
+  console.log(e);
+}
+
+async function edit_duration(e: any) {
+  const inputValue = e.detail.value;
+  const _duration = Number.isNaN(Number(inputValue)) ? 0 : Number(inputValue);
+  // const _duration = Number(e.detail.value);
+  console.log('_duration', _duration);
+  if (_duration < 20) {
+    uni.$u.toast('时长不能小于20分钟');
+    duration.value = 20;
+    return;
+  }
+  duration.value = _duration;
+}
+</script>
+
+<style lang="scss" scoped>
+.edit-course-time {
+  @apply flex-row flex items-center w-[180rpx] justify-between;
+}
+.btn {
+  @apply h-[50rpx] w-[50rpx] flex items-center justify-center bg-gray-50 text-[30rpx] text-black;
+}
+.course-title {
+  @apply flex flex-grow flex-shrink-0 justify-center items-center color-_wn_969799 mr-[20rpx];
+}
+.centered-input {
+  @apply text-center w-[80rpx] px-[10rpx];
+}
+</style>
